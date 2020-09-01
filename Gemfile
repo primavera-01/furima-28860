b5e553f0a3644d5136f85c4ec8c20ee8ba7ca379
primavera-01 <primavera0114@gmail.com>
2020-09-01 21:19:05 +0900

    バリデーションの設定

--- Gemfile
+++ Gemfile
@@ -54,7 +54,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
-
+gem 'pry-rails'
 group :development do
   gem 'rubocop', require: false
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :birth_day, presence: true
 end
