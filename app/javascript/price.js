76e9c74aff35e92bbe7cd5a513dc6672372420a7
primavera-01 <primavera0114@gmail.com>
2020-09-10 15:03:03 +0900

    js実装

---
+++ app/javascript/price.js
@@ -0,0 +1,11 @@
+function itemCalculation() {
+  const price = document.getElementById("item-price")
+  const itemPrice = document.getElementById("add-tax-price")
+  const itemProfit = document.getElementById("profit")
+  price.addEventListener("input", () =>{
+    const number = document.getElementById("item-price").value
+    itemPrice.innerHTML = Math.floor(number * 0.1);
+    itemProfit.innerHTML = Math.floor(number * 0.9);
+  });
+}
+window.addEventListener("load", itemCalculation);
